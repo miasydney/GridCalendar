b761f39a6e3ea174df4cc0778ae19e74fc60d9c8
mia <miabenn74@gmail.com>
2023-04-14 09:54:46 +1000

    Initial Commit

---
+++ GridCalendar/GridCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridCalendarApp.swift
+//  GridCalendar
+//
+//  Created by Mia on 14/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct GridCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
